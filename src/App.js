be92b3fa6a2331036eaa28dd98ec5f73e69d375a
Satyam Singh <singhsatyam2072@gmail.com>
2026-01-17 20:49:41 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Header from "./components/Header";
 import FoodOption from "./components/FoodOption";
 import GroceryOption from "./components/GroceryOption";
 import DineOption from "./components/DineOption";
+import Restaurant from "./components/Restaurant";
+
+
 function App(){
 
     return(
@@ -13,6 +16,7 @@ function App(){
         <FoodOption></FoodOption>
         <GroceryOption></GroceryOption>
         <DineOption></DineOption>
+        <Restaurant></Restaurant>
        </>
     )
 }
