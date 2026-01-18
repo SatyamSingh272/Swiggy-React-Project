c200d00e98bc8152382fb48d7b111fb759c8a570
Satyam Singh <singhsatyam2072@gmail.com>
2026-01-18 09:12:55 +0530

    data fetch successfully

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,28 +1,41 @@
 import { useEffect, useState } from "react";
-
+import RestCard from "./RestCard";
 
 export default function Restaurant(){
 
 
-     const [RestData, setRestData] = useState({})
+     const [RestData, setRestData] = useState([])
 
     useEffect(()=>{
 
 
         async function fetchData(){
 
+            const response = await fetch ("https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.52110&lng=73.85020&is-seo-homepage-enabled=true")
 
-           const proxyServer = "https://cors-anywhere.herokuapp.com/";
-           const swiggyAPI = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.52110&lng=73.85020&is-seo-homepage-enabled=true";
-           const response = await fetch(proxyServer+swiggyAPI)
-           const data = await response.json();
+            const data = await response.json();
+            setRestData(data);
 
-           setRestData(data.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+        //    const proxyServer = "https://cors-anywhere.herokuapp.com/";
+        //    const swiggyAPI = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.52110&lng=73.85020&is-seo-homepage-enabled=true";
+        //    const response = await fetch(proxyServer+swiggyAPI)
+        //    const data = await response.json();
+
+            setRestData(data.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
         }
         fetchData();
     },[])
 
-    console.log(RestData);
+    //console.log(RestData);
+
+    return(
+        <div className="flex flex-wrap w-[80%] mx-auto mt-20 gap-5">
+        {
+            RestData.map((restInfo)=><RestCard key={restInfo.info.id} restInfo={restInfo}></RestCard>)
+        }
+
+        </div>
+    )
 
     
 }
