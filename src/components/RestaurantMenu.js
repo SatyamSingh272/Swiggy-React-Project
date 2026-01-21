951455efb8b29a1ea1a704862107ccc51c019656
Satyam Singh <singhsatyam2072@gmail.com>
2026-01-21 23:22:29 +0530

    menu page is added

---
+++ src/components/RestaurantMenu.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+
+export default function RestaurantMenu(){
+
+    let {id}= useParams();
+    console.log(id);
+
+    const [RestData, setRestData] = useState(null);
+
+    useEffect(()=>{
+
+
+        async function fetchData(){
+
+            const response = await fetch (`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.52110&lng=73.85020&restaurantId=${id}&catalog_qa=undefined&submitAction=ENTER`)
+
+            const data = await response.json();
+            setRestData(data);
+
+
+            setRestData(data);
+        }
+        fetchData();
+    },[])
+
+    console.log(RestData);
+
+    return(
+        <h1>Hello World</h1>
+    )
+
+}
