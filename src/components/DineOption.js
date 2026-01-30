2b26d7fb60b74b6830a037d860851ccf2f2f5d0e
Satyam Singh <singhsatyam2072@gmail.com>
2026-01-30 22:45:14 +0530

    completed

--- src/components/DineOption.js
+++ src/components/DineOption.js
@@ -1,20 +1,18 @@
-import { dineoutRestaurants } from "../utils/DineData"
+import {dineoutRestaurants} from "../utils/DineData"
 import DineCard from "./DineCard"
 
 
 export default function DineOption(){
-    return(
-        <>
+
+    return (
         <div className="w-[80%] mx-auto mt-20 mb-20">
-            <h1 className="font-bold text-2xl">Discover best restaurants on Dineout</h1>
-            <div className="flex flex-nowrap overflow-x-auto mt-5 gap-4">
-                {
-                    dineoutRestaurants.map((RestData)=><DineCard key={RestData?.info?.id} RestData={RestData}></DineCard>)
-                }
+            <p className="text-3xl font-bold">Discover best restaurants on Dineout</p>
+            <div className="flex flex-nowrap overflow-x-auto mt-5 gap-4 ">
+               {
+                dineoutRestaurants.map((RestData)=><DineCard key={RestData?.info?.id} RestData={RestData}></DineCard>)
+               }
             </div>
+
         </div>
-
-        </>
     )
-
 }
