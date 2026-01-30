85d57357538d0afe031f1cef4a44c929cec6d8d2
Satyam Singh <singhsatyam2072@gmail.com>
2026-01-30 22:43:58 +0530

    completed

--- src/components/DineCard.js
+++ src/components/DineCard.js
@@ -1,36 +1,32 @@
 
-
 export default function DineCard({RestData}){
-    return(
-    <div className="max-w-sm flex-none">
-    <a target="_blank" href={RestData?.cta?.link}>
-  <div className="relative w-80 h-50">
-    
-    {/* Image */}
-    <img
-      className="w-full h-full object-cover rounded-lg"
-      src={"https://media-assets.swiggy.com/swiggy/image/upload/" + RestData?.info?.mediaFiles[0]?.url}
-      alt="restaurant"
-    />
 
-    {/* Dark Gradient Overlay */}
-    <div className="absolute inset-0 bg-linear-to-t from-black/60 via-black/20 to-transparent rounded-lg"></div>
 
-    {/* Restaurant Name */}
-    <p className="absolute bottom-2 left-2 text-lg font-bold text-white drop-shadow-lg z-10">
-      {RestData?.info?.name}
-    </p>
-
-    {/* Rating */}
-    <p className="absolute bottom-2 right-2 text-lg font-bold text-white drop-shadow-lg z-10">
-    {RestData?.info?.rating?.value}
-    </p>
-
-  </div>
-  </a>
-</div>
-
+    return (
+        <div className="max-w-sm flex-none">
+       <a target="_blank" href={RestData.cta.link}>
+        <div className="relative">
+          <img
+            className="w-80 h-50 object-cover"
+            src={
+              "https://media-assets.swiggy.com/swiggy/image/upload/" +
+              RestData?.info?.mediaFiles[0]?.url
+            }
+            alt="Restaurant"
+          />
+          {/* Gradient overlay behind the text */}
+          <div className="absolute bottom-0 left-0 right-0 h-16 bg-gradient-to-t from-black to-transparent"></div>
+          {/* Text with higher z-index */}
+          <p className="absolute bottom-2 left-2 text-xl text-white z-10">
+            {RestData.info.name}
+          </p>
+          <p className="absolute bottom-2 right-2 text-xl text-white z-10">
+            {RestData?.info?.rating?.value}
+          </p>
+        </div>
+        </a>
+      </div>
+      
     )
-
 
 }
