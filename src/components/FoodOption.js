c51ef72a8f8cd9ab9054c9ba1435131cdeaa9213
Satyam Singh <singhsatyam2072@gmail.com>
2026-01-30 22:48:49 +0530

    completed

--- src/components/FoodOption.js
+++ src/components/FoodOption.js
@@ -1,27 +1,15 @@
-import { imageGridCards } from "../utils/FoodData"
-import FoodCard from "./FoodCard"
+import { imageGridCards } from "../utils/foodData"
+import Foodcard from "./FoodCard"
 
 export default function FoodOption(){
 
-    return(
+    return (
         <>
-    
-
-           <div className="w-[80%] container mx-auto mt-15">
-           <h1 className="text-2xl font-bold">Order our best food options</h1>
-
-           <div className="overflow-x-auto mt-5">
-           <div className="grid grid-flow-col grid-rows-2 gap-5 w-max">
-             {imageGridCards.map((foodData) => (
-             <FoodCard key={foodData.id} foodData={foodData} />
-          ))}
+          <div className="w-[80%] container mx-auto flex flex-wrap mt-20 gap-3">
+            {
+                imageGridCards.map((foodData)=><Foodcard key={foodData.id} foodData={foodData}></Foodcard>)
+            }
           </div>
-          </div>
-          </div>
-
-
-
         </>
     )
-
 }
