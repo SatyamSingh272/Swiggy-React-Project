1ef2effbdfaeeb678573c310a81a529bffedfbe5
Satyam Singh <singhsatyam2072@gmail.com>
2026-02-02 22:57:26 +0530

    shimmer effect apply

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,143 +1,118 @@
 
-
 export default function Shimmer(){
+   
+    return (
+        <>
+         <div className="flex flex-wrap w-[80%] mx-auto mt-20 gap-5">
+            <div className="w-70 mb-2">
+              <div className="w-70 h-45 rounded-xl bg-gray-300"></div>
+              <div className="w-[95%] mx-auto mt-3">
+                <div className="w-full h-6 bg-gray-300"></div>
+                <div className="w-full h-6 bg-gray-300 mt-2"></div>
+                <div className="w-full h-6 bg-gray-300 mt-2"></div>
+              </div>
 
-  return(
-    <>
-      <div className="flex flex-wrap w-[80%] mx-auto mt-20 gap-5">
-        <div className="w-70 mb-2">
-          <div className="w-70 h-45 rounded-xl bg-gray-400"></div>
-          <div className="w-[95%] mx-auto mt-3">
-            <div className="w-full h-6 bg-gray-400"></div>
-            <div className="w-full h-6 bg-gray-400 mt-2"></div>
-            <div className="w-full h-6 bg-gray-400 mt-2"></div>
-          </div>
-        </div>
+            </div>
+            <div className="w-70 mb-2">
+              <div className="w-70 h-45 rounded-xl bg-gray-300"></div>
+              <div className="w-[95%] mx-auto mt-3">
+                <div className="w-full h-6 bg-gray-300"></div>
+                <div className="w-full h-6 bg-gray-300 mt-2"></div>
+                <div className="w-full h-6 bg-gray-300 mt-2"></div>
+              </div>
 
-         <div className="w-70 mb-2">
-          <div className="w-70 h-45 rounded-xl bg-gray-400"></div>
-          <div className="w-[95%] mx-auto mt-3">
-            <div className="w-full h-6 bg-gray-400"></div>
-            <div className="w-full h-6 bg-gray-400 mt-2"></div>
-            <div className="w-full h-6 bg-gray-400 mt-2"></div>
-          </div>
-        </div>
+            </div>
+            <div className="w-70 mb-2">
+              <div className="w-70 h-45 rounded-xl bg-gray-300"></div>
+              <div className="w-[95%] mx-auto mt-3">
+                <div className="w-full h-6 bg-gray-300"></div>
+                <div className="w-full h-6 bg-gray-300 mt-2"></div>
+                <div className="w-full h-6 bg-gray-300 mt-2"></div>
+              </div>
 
-         <div className="w-70 mb-2">
-          <div className="w-70 h-45 rounded-xl bg-gray-400"></div>
-          <div className="w-[95%] mx-auto mt-3">
-            <div className="w-full h-6 bg-gray-400"></div>
-            <div className="w-full h-6 bg-gray-400 mt-2"></div>
-            <div className="w-full h-6 bg-gray-400 mt-2"></div>
-          </div>
-        </div>
+            </div>
+            <div className="w-70 mb-2">
+              <div className="w-70 h-45 rounded-xl bg-gray-300"></div>
+              <div className="w-[95%] mx-auto mt-3">
+                <div className="w-full h-6 bg-gray-300"></div>
+                <div className="w-full h-6 bg-gray-300 mt-2"></div>
+                <div className="w-full h-6 bg-gray-300 mt-2"></div>
+              </div>
 
-         <div className="w-70 mb-2">
-          <div className="w-70 h-45 rounded-xl bg-gray-400"></div>
-          <div className="w-[95%] mx-auto mt-3">
-            <div className="w-full h-6 bg-gray-400"></div>
-            <div className="w-full h-6 bg-gray-400 mt-2"></div>
-            <div className="w-full h-6 bg-gray-400 mt-2"></div>
-          </div>
-        </div>
+            </div>
+            <div className="w-70 mb-2">
+              <div className="w-70 h-45 rounded-xl bg-gray-300"></div>
+              <div className="w-[95%] mx-auto mt-3">
+                <div className="w-full h-6 bg-gray-300"></div>
+                <div className="w-full h-6 bg-gray-300 mt-2"></div>
+                <div className="w-full h-6 bg-gray-300 mt-2"></div>
+              </div>
 
-         <div className="w-70 mb-2">
-          <div className="w-70 h-45 rounded-xl bg-gray-400"></div>
-          <div className="w-[95%] mx-auto mt-3">
-            <div className="w-full h-6 bg-gray-400"></div>
-            <div className="w-full h-6 bg-gray-400 mt-2"></div>
-            <div className="w-full h-6 bg-gray-400 mt-2"></div>
-          </div>
-        </div>
+            </div>
+            <div className="w-70 mb-2">
+              <div className="w-70 h-45 rounded-xl bg-gray-300"></div>
+              <div className="w-[95%] mx-auto mt-3">
+                <div className="w-full h-6 bg-gray-300"></div>
+                <div className="w-full h-6 bg-gray-300 mt-2"></div>
+                <div className="w-full h-6 bg-gray-300 mt-2"></div>
+              </div>
 
-         <div className="w-70 mb-2">
-          <div className="w-70 h-45 rounded-xl bg-gray-400"></div>
-          <div className="w-[95%] mx-auto mt-3">
-            <div className="w-full h-6 bg-gray-400"></div>
-            <div className="w-full h-6 bg-gray-400 mt-2"></div>
-            <div className="w-full h-6 bg-gray-400 mt-2"></div>
-          </div>
-        </div>
+            </div>
+            <div className="w-70 mb-2">
+              <div className="w-70 h-45 rounded-xl bg-gray-300"></div>
+              <div className="w-[95%] mx-auto mt-3">
+                <div className="w-full h-6 bg-gray-300"></div>
+                <div className="w-full h-6 bg-gray-300 mt-2"></div>
+                <div className="w-full h-6 bg-gray-300 mt-2"></div>
+              </div>
 
+            </div>
+            <div className="w-70 mb-2">
+              <div className="w-70 h-45 rounded-xl bg-gray-300"></div>
+              <div className="w-[95%] mx-auto mt-3">
+                <div className="w-full h-6 bg-gray-300"></div>
+                <div className="w-full h-6 bg-gray-300 mt-2"></div>
+                <div className="w-full h-6 bg-gray-300 mt-2"></div>
+              </div>
 
-         <div className="w-70 mb-2">
-          <div className="w-70 h-45 rounded-xl bg-gray-400"></div>
-          <div className="w-[95%] mx-auto mt-3">
-            <div className="w-full h-6 bg-gray-400"></div>
-            <div className="w-full h-6 bg-gray-400 mt-2"></div>
-            <div className="w-full h-6 bg-gray-400 mt-2"></div>
-          </div>
-        </div>
+            </div>
+            <div className="w-70 mb-2">
+              <div className="w-70 h-45 rounded-xl bg-gray-300"></div>
+              <div className="w-[95%] mx-auto mt-3">
+                <div className="w-full h-6 bg-gray-300"></div>
+                <div className="w-full h-6 bg-gray-300 mt-2"></div>
+                <div className="w-full h-6 bg-gray-300 mt-2"></div>
+              </div>
 
-         <div className="w-70 mb-2">
-          <div className="w-70 h-45 rounded-xl bg-gray-400"></div>
-          <div className="w-[95%] mx-auto mt-3">
-            <div className="w-full h-6 bg-gray-400"></div>
-            <div className="w-full h-6 bg-gray-400 mt-2"></div>
-            <div className="w-full h-6 bg-gray-400 mt-2"></div>
-          </div>
-        </div>
+            </div>
+            <div className="w-70 mb-2">
+              <div className="w-70 h-45 rounded-xl bg-gray-300"></div>
+              <div className="w-[95%] mx-auto mt-3">
+                <div className="w-full h-6 bg-gray-300"></div>
+                <div className="w-full h-6 bg-gray-300 mt-2"></div>
+                <div className="w-full h-6 bg-gray-300 mt-2"></div>
+              </div>
 
+            </div>
+            <div className="w-70 mb-2">
+              <div className="w-70 h-45 rounded-xl bg-gray-300"></div>
+              <div className="w-[95%] mx-auto mt-3">
+                <div className="w-full h-6 bg-gray-300"></div>
+                <div className="w-full h-6 bg-gray-300 mt-2"></div>
+                <div className="w-full h-6 bg-gray-300 mt-2"></div>
+              </div>
 
-         <div className="w-70 mb-2">
-          <div className="w-70 h-45 rounded-xl bg-gray-400"></div>
-          <div className="w-[95%] mx-auto mt-3">
-            <div className="w-full h-6 bg-gray-400"></div>
-            <div className="w-full h-6 bg-gray-400 mt-2"></div>
-            <div className="w-full h-6 bg-gray-400 mt-2"></div>
-          </div>
-        </div>
+            </div>
+            <div className="w-70 mb-2">
+              <div className="w-70 h-45 rounded-xl bg-gray-300"></div>
+              <div className="w-[95%] mx-auto mt-3">
+                <div className="w-full h-6 bg-gray-300"></div>
+                <div className="w-full h-6 bg-gray-300 mt-2"></div>
+                <div className="w-full h-6 bg-gray-300 mt-2"></div>
+              </div>
 
-
-         <div className="w-70 mb-2">
-          <div className="w-70 h-45 rounded-xl bg-gray-400"></div>
-          <div className="w-[95%] mx-auto mt-3">
-            <div className="w-full h-6 bg-gray-400"></div>
-            <div className="w-full h-6 bg-gray-400 mt-2"></div>
-            <div className="w-full h-6 bg-gray-400 mt-2"></div>
-          </div>
-        </div>
-
-         <div className="w-70 mb-2">
-          <div className="w-70 h-45 rounded-xl bg-gray-400"></div>
-          <div className="w-[95%] mx-auto mt-3">
-            <div className="w-full h-6 bg-gray-400"></div>
-            <div className="w-full h-6 bg-gray-400 mt-2"></div>
-            <div className="w-full h-6 bg-gray-400 mt-2"></div>
-          </div>
-        </div>
-
-         <div className="w-70 mb-2">
-          <div className="w-70 h-45 rounded-xl bg-gray-400"></div>
-          <div className="w-[95%] mx-auto mt-3">
-            <div className="w-full h-6 bg-gray-400"></div>
-            <div className="w-full h-6 bg-gray-400 mt-2"></div>
-            <div className="w-full h-6 bg-gray-400 mt-2"></div>
-          </div>
-        </div>
-
-         <div className="w-70 mb-2">
-          <div className="w-70 h-45 rounded-xl bg-gray-400"></div>
-          <div className="w-[95%] mx-auto mt-3">
-            <div className="w-full h-6 bg-gray-400"></div>
-            <div className="w-full h-6 bg-gray-400 mt-2"></div>
-            <div className="w-full h-6 bg-gray-400 mt-2"></div>
-          </div>
-        </div>
-
-         <div className="w-70 mb-2">
-          <div className="w-70 h-45 rounded-xl bg-gray-400"></div>
-          <div className="w-[95%] mx-auto mt-3">
-            <div className="w-full h-6 bg-gray-400"></div>
-            <div className="w-full h-6 bg-gray-400 mt-2"></div>
-            <div className="w-full h-6 bg-gray-400 mt-2"></div>
-          </div>
-        </div>
-
-        
-      </div>
-
-     
-    </>
-  )
+            </div>
+         </div>
+        </>
+    )
 }
